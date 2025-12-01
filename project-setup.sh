2cf6e65640274bd777f2eb5013b29cf46fb274d7
Szilard Nemeth <szilard.nemeth88@gmail.com>
2025-12-01 18:01:21 -0500

    project-setup.sh: Simplify bash script

--- project-setup.sh
+++ project-setup.sh
@@ -1,25 +1,8 @@
 #!/usr/bin/env bash
 
-function setup-vars {
-    # Replace this with the dir of your choice
-    PROJECT_REPO_ROOT=""
-    export PROJECT_REPO_ROOT="$HOME/development/my-repos/trello-backup/"
-}
-
 function trello-backup {
-    ORIG_PYTHONPATH=$PYTHONPATH
-    unset PYTHONPATH
-
-    cd $PROJECT_REPO_ROOT
-    venv_path="$(poetry env info --path)""/bin/activate"
-
-    source $venv_path
-    python -m trello_backup.cli.cli "$@"
-
-    # Cleanup
-    deactivate
-    PYTHONPATH=$ORIG_PYTHONPATH
+    PROJECT_REPO_ROOT="$HOME/development/my-repos/trello-backup/"
+    cd $PROJECT_REPO_ROOT && poetry run python trello_backup/cli/cli.py "$@"
 }
 
-## Execute setup
-setup-vars
+trello_backup
